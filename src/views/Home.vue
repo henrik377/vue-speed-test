aceefd2fb5df264aab3c4db4eafeeaab118077cc
Henrik Stridsman <henrik@377.se>
2021-06-14 11:07:37 +0200

    heroku info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <a href="/about">Kuken</a>
+    </div>
+    <div
+      v-for="index in 1000"
+      :key="index"
+      v-once>
+      <a href="/about">{{ index }}</a>
+    </div>
   </div>
 </template>
 
@@ -13,6 +20,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted(){
+    this.axios.get('https://dev.bongobongo.ug/api/uof/match/event/ListAllEvents?includePreEvents=true&nrOfPre=20&SportId=1&marketId=1&lastKey=&siteid=1&timeOffset=180')
   }
 }
 </script>
