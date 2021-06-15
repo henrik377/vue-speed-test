709ba7ea046db8215207188faa8757dd44c5d457
Henrik Stridsman <henrik@377.se>
2021-06-15 09:09:02 +0200

    OPS - Fewer nodes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@ export default {
     HelloWorld
   },
   mounted(){
-    this.ii = 1000
+    this.ii = 200
     //this.axios.get('https://dev.bongobongo.ke/api/uof/match/event/ListAllEvents?includePreEvents=true&nrOfPre=20&SportId=1&marketId=1&lastKey=&siteid=1&timeOffset=180')
   }
 }
